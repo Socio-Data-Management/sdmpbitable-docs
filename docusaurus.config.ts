4ebd493793a66626f234673576d3f085f7065054
Olivier Hennebelle <olivier.hennebelle@socio-dm.com>
2026-07-07 18:33:59 +0200

    Custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,10 +30,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://socio-data-management.github.io',
+  url: 'https://docs-table.socio-dm.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sdmpbitable-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
